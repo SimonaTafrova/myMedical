d02a32ab7b51815baa7f32bda78623b505cdc566
Simona Tafrova <simonatafrova@gmail.com>
2024-06-04 14:52:09 +0300

    Change a textview content

--- app/src/main/java/com/example/mymedical/LoginActivity.java
+++ app/src/main/java/com/example/mymedical/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.mymedical;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +14,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText editUsername, editPassword;
+    Button btn;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        editUsername = findViewById(R.id.editTextUsername);
+        editPassword = findViewById(R.id.editTextPassword);
+        btn = findViewById(R.id.LoginButton);
+        tv = findViewById(R.id.textViewSignUp);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
