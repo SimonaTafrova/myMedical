cc2008392a173a1449a89e24e0855da8737bd155
Simona Tafrova <simonatafrova@gmail.com>
2024-06-08 15:44:14 +0300

    Implemented Logout function in Home Activity

--- app/src/main/java/com/example/mymedical/HomeActivity.java
+++ app/src/main/java/com/example/mymedical/HomeActivity.java
@@ -1,9 +1,15 @@
 package com.example.mymedical;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,6 +20,22 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username","").toString();
+        Toast.makeText(getApplicationContext(), "Welcome " + username, Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.cardLogout);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent( HomeActivity.this, LoginActivity.class));
+            }
+        });
+
 
     }
 }
