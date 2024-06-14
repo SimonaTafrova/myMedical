3b5d398d9567a6bf8b4b038bc9c5babfdcbd6c38
Simona Tafrova <simonatafrova@gmail.com>
2024-06-14 16:55:46 +0300

    Started implementing GPActivity input logic. Made sure empty data fields can't be inserted into db.

--- app/src/main/java/com/example/mymedical/GPActivity.java
+++ app/src/main/java/com/example/mymedical/GPActivity.java
@@ -3,7 +3,10 @@ package com.example.mymedical;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GPActivity extends AppCompatActivity {
     ImageView back;
+    EditText edDoctor, edTopic;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_gpactivity);
+
+        edDoctor = findViewById(R.id.editTextDoctorName);
+        edTopic = findViewById(R.id.editTextCallTopic);
+        button = findViewById(R.id.createDoctorLogButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewLog(edDoctor.getText().toString(),edTopic.getText().toString());
+            }
+        });
 
         back = findViewById(R.id.backArrowGP);
         back.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +45,11 @@ public class GPActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void createNewLog(String doctor, String topic){
+        if(doctor.length() == 0 || topic.length() == 0){
+            Toast.makeText(getApplicationContext(),"All fields are required", Toast.LENGTH_SHORT).show();
+        }
     }
 }
