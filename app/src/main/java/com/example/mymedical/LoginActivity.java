92202122139cf7df10723eb4345223ced5d4c35e
Simona Tafrova <simonatafrova@gmail.com>
2024-07-05 22:46:03 +0300

    Improved the code quality of LoginActivity.java

--- app/src/main/java/com/example/mymedical/LoginActivity.java
+++ app/src/main/java/com/example/mymedical/LoginActivity.java
@@ -1,15 +1,12 @@
 package com.example.mymedical;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -32,40 +29,34 @@ public class LoginActivity extends AppCompatActivity {
         btn = findViewById(R.id.LoginButtonn);
         tv = findViewById(R.id.textViewSignUp);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = editUsername.getText().toString();
-                String password = editPassword.getText().toString();
-                Database database = new Database(getApplicationContext(),"myMedical", null, 1);
-                if(username.length() == 0 || password.length() == 0){
-                    Toast.makeText(getApplicationContext(), "All fields are required!", Toast.LENGTH_SHORT).show();
+        btn.setOnClickListener(v -> {
+            String username = editUsername.getText().toString();
+            String password = editPassword.getText().toString();
+            Database database = new Database(getApplicationContext(),"myMedical", null, 1);
+            if(username.isEmpty() || password.isEmpty()){
+                Toast.makeText(getApplicationContext(), "All fields are required!", Toast.LENGTH_SHORT).show();
+            } else {
+                if(database.login(username,password) == 1){
+                Toast.makeText(getApplicationContext(),"Successful Login!", Toast.LENGTH_SHORT).show();
+                    String fullName = database.getFullName(username);
+                    SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("fullName", fullName);
+
+                    editor.apply();
+                    startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                 } else {
-                    if(database.login(username,password) == 1){
-                    Toast.makeText(getApplicationContext(),"Successful Login!", Toast.LENGTH_SHORT).show();
-                        String fullname = database.getFullName(username);
-                        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putString("fullName", fullname);
-
-                        editor.apply();
-                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-                    } else {
-                        Toast.makeText(getApplicationContext(),"Wrong username or password!", Toast.LENGTH_SHORT).show();
-
-                    }
+                    Toast.makeText(getApplicationContext(),"Wrong username or password!", Toast.LENGTH_SHORT).show();
 
                 }
 
             }
+
         });
 
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
+        tv.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
