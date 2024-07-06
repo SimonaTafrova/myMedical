3238ef391efb3a424ddda03f0ffb0fe90f654aef
Simona Tafrova <simonatafrova@gmail.com>
2024-07-06 22:39:54 +0300

    Improved the code quality of StatisticsActivity.java

--- app/src/main/java/com/example/mymedical/StatisticsActivity.java
+++ app/src/main/java/com/example/mymedical/StatisticsActivity.java
@@ -1,23 +1,16 @@
 package com.example.mymedical;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import java.util.List;
 
 public class StatisticsActivity extends AppCompatActivity {
     int sensorsCount = 0;
-    int perscriptionCount = 0;
+    int prescriptionCount = 0;
     int gpCallsCount = 0;
     TextView sensorView, prescriptionView, doctorCallsView;
 
@@ -30,42 +23,44 @@ public class StatisticsActivity extends AppCompatActivity {
         sensorView = findViewById(R.id.SensorsCountTextView);
         prescriptionView = findViewById(R.id.PerscriptionCountTextView);
         doctorCallsView = findViewById(R.id.GpCallCountTextView);
+        ImageView back = findViewById(R.id.backArrowStat);
 
         Database database = new Database(getApplicationContext(),"myMedical", null, 1);
 
-        //perscriptionCount = findViewById(R.id.PerscriptionCountTextView);
-        //sensorsCount = findViewById(R.id.SensorsCountTextView);
+        getCounters(database);
+        setCounters(sensorView, prescriptionView, doctorCallsView);
 
+        back.setOnClickListener(v -> startActivity(new Intent(StatisticsActivity.this, HomeActivity.class)));
+
+    }
+
+    private void setCounters(TextView sensorView, TextView prescriptionView, TextView doctorCallsView) {
+
+        sensorView.setText(String.valueOf(sensorsCount));
+        prescriptionView.setText(String.valueOf(prescriptionCount));
+        doctorCallsView.setText(String.valueOf(gpCallsCount));
+
+    }
+
+    private void getCounters(Database database) {
         List<Event> allEvents = database.getAllEvents();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             allEvents
                     .forEach(e -> {
-                        if(e.getMessage().equals("Started new sensor")){
-                            sensorsCount++;
-                        } else if(e.getMessage().equals("Collected a new perscription")){
-                            perscriptionCount++;
-                        } else if(e.getMessage().equals("A new doctor call recorded!")){
-                            gpCallsCount++;
-                        };
+                        switch (e.getMessage()) {
+                            case "Started new sensor":
+                                sensorsCount++;
+                                break;
+                            case "Collected a new prescription":
+                                prescriptionCount++;
+                                break;
+                            case "A new doctor call recorded!":
+                                gpCallsCount++;
+                                break;
+                        }
+
                     });
         }
-
-
-        sensorView.setText(String.valueOf(sensorsCount));
-        prescriptionView.setText(String.valueOf(perscriptionCount));
-        doctorCallsView.setText(String.valueOf(gpCallsCount));
-
-
-         ImageView back = findViewById(R.id.backArrowStat);
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(StatisticsActivity.this, HomeActivity.class));
-            }
-        });
-
-
     }
 }
